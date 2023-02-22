3db5b385d44bdb7b912387082d7372eb51aee87c
Kiranchavan05 <kiranchavan0502@gmail.com>
2023-02-22 13:53:56 +0530

    my first new commit to git

---
+++ kiranfirstdom.js
@@ -0,0 +1,7 @@
+console.log(document.getElementById('header-Text'));
+var header=document.getElementById('main-header');
+header.style.borderBottom='solid 3px #000';
+var headerTitle=document.getElementById('header-title');
+console.log(headerTitle);
+
+
